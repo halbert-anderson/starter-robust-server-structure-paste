ceba6a6f42d3193e7884a070e4f909fdc1b9f12e
Hal Anderson <halbert.anderson@verizon.net>
2024-02-21 13:34:35 -0500

    Usig RESTful design principles added seperate GET and POST methods to the /pastes path and added HTTP response status codes to responses

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ const app = express();
 // TODO: Follow instructions in the checkpoint to implement ths API.
 const pastes = require("./data/pastes-data");
 
+// The express.json() function is a built-in middleware that adds
+// a body property to the request (req.body). 
+app.use(express.json());
+
 app.use("/pastes/:pasteId", (req, res, next) => {
   const { pasteId } = req.params;
   const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
@@ -15,10 +19,41 @@ app.use("/pastes/:pasteId", (req, res, next) => {
     }
   });
 
-app.use("/pastes", (req, res) => {
+//app.use("/pastes", (req, res) => {
+  app.get("/pastes", (req, res) => {
   res.json({ data: pastes });
   //res.send({ data: pastes });
 });
+
+// Variable to hold the next ID
+// Because some IDs may already be used, find the largest assigned ID
+let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
+
+app.post("/pastes", (req, res, next) => {
+
+  const { data: { name, syntax, exposure, expiration, text, user_id } = {} } = req.body;
+  if(text){
+  const newPaste = {
+    id: ++lastPasteId, // Increment last ID, then assign as the current ID
+    name,
+    syntax,
+    exposure,
+    expiration,
+    text,
+    user_id,
+  };
+  pastes.push(newPaste);
+
+  //reutrns an HTTP request error code of 201 if paste is successfully created
+  //res.json({ data: newPaste });
+    res.status(201).json({ data: newPaste });
+}else {
+  // posting an object with a missing or empty text property will return 400.
+  res.sendStatus(400);
+   }
+});
+
+
 
 // Not found handler
 app.use((request, response, next) => {
---
+++ tests/app.test.js
@@ -0,0 +1,10 @@
+const request = require("supertest");
+const pastes = require("../src/data/pastes-data");
+const app = require("../src/app");
+
+
+describe("path /pastes", () => {
+
+  // Add tests here
+
+});
